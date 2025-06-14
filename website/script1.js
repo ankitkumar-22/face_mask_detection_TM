6a40e2f44ec96d5e4067d1d0ac04d8f91c97887a
viditShar <73697299+viditShar@users.noreply.github.com>
2025-06-14 13:17:40 +0530

    Add files via upload

---
+++ website/script1.js
@@ -0,0 +1,33 @@
+let currentImages = [];
+
+setInterval(async () => {
+  try {
+    const res = await fetch('/get-latest-images');
+    const latestImages = await res.json();
+    const gallery = document.getElementById('gallery');
+
+    // Remove images that are no longer in the list
+    currentImages.forEach(src => {
+      if (!latestImages.includes(src)) {
+        const img = [...gallery.getElementsByTagName('img')]
+                      .find(img => img.src === location.origin + src);
+        if (img) img.remove();
+      }
+    });
+
+    // Add new images
+    latestImages.forEach(src => {
+      if (!currentImages.includes(src)) {
+        const img = document.createElement('img');
+        img.src = src;
+        img.width = 200;
+        img.style.margin = "10px";
+        gallery.prepend(img);
+      }
+    });
+
+    currentImages = latestImages;
+  } catch (err) {
+    console.error('Error updating gallery:', err);
+  }
+}, 5000);
